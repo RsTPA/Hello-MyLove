afacde43017dab2ad5e400caf3ec6ca7bbe77fe3
Pavel <161854845+RsTPA@users.noreply.github.com>
2024-03-03 20:01:47 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function(){
+    output.innerHTML = 'Зая, я тебя люблю 🫶🏽';
+});
